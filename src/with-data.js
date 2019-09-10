694691f27a8e9b7e3a814a327a31e3de8cf91f3c
Sergey Lem <sergey.lem@gmail.com>
2019-09-10 15:27:36 +0600

    added user-list, user-profile, with-data

---
+++ src/with-data.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const withData = WrappedComponent => {
+  class WithData extends React.Component {
+      state = {
+        data: []
+      };
+
+    componentDidMount() {
+      setTimeout(() => {
+        fetch(this.props.dataSource)
+          .then(response => response.json())
+          .then(data => this.setState({ data: data.slice(0, 3) }));
+      }, 1000);
+    }
+
+    render() {
+      const { dataSource , ...otherProps } = this.props;
+
+      return this.state.data.length < 1 ? (
+        <h1>LOADING</h1>
+      ) : (
+        <WrappedComponent data={this.state.data} {...otherProps} />
+      );
+    }
+  }
+
+  return WithData;
+};
+
+export default withData;
